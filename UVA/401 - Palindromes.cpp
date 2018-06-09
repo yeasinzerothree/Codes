4cab64c2d97987b30962eddb8d616166681eda58
Yeasin Mollik <39093353+yeasinzerothree@users.noreply.github.com>
2018-06-09 21:42:05 +0600

    Solution

---
+++ UVA/401 - Palindromes.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    return s == string(s.rbegin(), s.rend());
+}
+
+bool isMirrored(string s) {
+    string s1;
+
+    for (int i = 0; i < s.size(); ++i) {
+
+        if (s[i] == 'U' || s[i] == 'V' || s[i] == 'W' || s[i] == 'X' || s[i] == 'Y' || s[i] == '1' || s[i] == '8' ||
+            s[i] == 'A' || s[i] == 'I' || s[i] == 'M' || s[i] == 'O' || s[i] == 'T' || s[i] == 'H')
+            s1 += s[i];
+       else if (s[i] == 'E')
+            s1 += '3';
+        else if (s[i] == 'J')
+            s1 += 'L';
+        else if (s[i] == 'L')
+            s1 += 'J';
+        else if (s[i] == 'S')
+            s1 += '2';
+        else if (s[i] == 'Z')
+            s1 += '5';
+        else if (s[i] == '2')
+            s1 += 'S';
+        else if (s[i] == '3')
+            s1 += 'E';
+        else if (s[i] == '5')
+            s1 += 'Z';
+        else
+            return false;
+    }
+
+    return s1 == string(s.rbegin(), s.rend());
+}
+
+int main() {
+    string s;
+    int t = 1;
+
+    while (cin >> s) {
+        if (t != 1)
+            cout << endl;
+        if (isPalindrome(s) && isMirrored(s))
+            cout << s << " -- is a mirrored palindrome." << endl;
+        else if (isPalindrome(s) && !isMirrored(s))
+            cout << s << " -- is a regular palindrome." << endl;
+        else if (isMirrored(s) && !isPalindrome(s))
+            cout << s << " -- is a mirrored string." << endl;
+        else
+            cout << s << " -- is not a palindrome." << endl;
+        t++;
+    }
+
+}
